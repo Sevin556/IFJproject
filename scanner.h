5b97bc46db2a96a5e1a285c41a826f9610fe50f7
expers-vis <32218820+expers-vis@users.noreply.github.com>
2019-11-11 16:57:48 +0100

    pridané stavy KA

--- scanner.h
+++ scanner.h
@@ -1,28 +1,82 @@
 /*
-* Predmet  :   IFJ / IAL
-* Súbor    :   scanner.c - Implementácia lexikálneho analyzátora
-* Projekt  :   Implementácia prekladača imperatívneho jazyka IFJ19
-* Tým č    :   127
-* Varianta :   I
-* Autoři   : xhalom00, Ivan Halomi
-*            xhiner00, Martin Hiner
-*            xsevci64, Adam Ševčík
-*            xzakji02, Jiří Žák
-*/
+ * Predmet  :   IFJ / IAL
+ * Súbor    :   scanner.c - Implementácia lexikálneho analyzátora
+ * Projekt  :   Implementácia prekladača imperatívneho jazyka IFJ19
+ * Tým č    :   127
+ * Varianta :   I
+ * Autoři   : xhalom00, Ivan Halomi
+ *            xhiner00, Martin Hiner
+ *            xsevci64, Adam Ševčík
+ *            xzakji02, Jiří Žák
+ */
 
 #ifndef _SCANNER_H_
 #define _SCANNER_H_
 
-/* štrukturovaný typ tToken */
-typedef struct {
-  int type;         /* typ lexému */
-  string lexeme;    /* obsah lexémy */
-}tToken;
+#include "string.h"
 
+/* štrukturovaný dátový typ tToken */
+typedef struct {
+        int type;   /* typ lexému */
+        string data; /* obsah lexémy */
+} tToken;
 
 
 typedef enum {
-  tStart
+        sStart,             /* počiatočný stav */
+
+        sEOL,
+        sEOF,
+
+        sIndent,
+        sDedent,
+        sDentDecide,
+        sDentEnd,
+
+        sMore,              /*  */
+        sMoreEqual,
+        sLess,
+        sLessEqual,
+        sInequal,
+        sInequalMid,
+        sEqual,
+        sAssignment,
+        sDivideFloat,
+        sDivideInteger,
+        sMultiplication,
+        sMinus,
+        sPlus,
+
+        sSemicolon,
+        sComma,
+        sLeftBracket,
+        sRightBracket,
+
+        sIdentificatorOrKeyword,
+        sIdentificator,
+        sKeyword,
+
+        sLineComment,
+        sBlockComment,
+
+        sStringStart,
+        sStringRead,
+        sStringEscape,
+        sStringEscapeHex,
+        sStringEscapeHexValue,
+        sString,
+
+        sIntegerOrDouble,
+        sInteger,
+        sDoublePoint,
+        sDoublePointNumber,
+        sDoubleExponent,
+        sDoubleExponentOperator,
+        sDoubleExponentNumber
+
 } tState;
+
+
+tToken get_token(void);
 
 #endif  //_SCANNER_H_
