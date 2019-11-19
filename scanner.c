97f24a3c092a7714f22e655a110b7d0a33178c64
expers-vis <32218820+expers-vis@users.noreply.github.com>
2019-11-19 20:38:22 +0100

    moc vecí
    návratový typ je teraz *tToken
    pridané funkcie na inicializáciu tokenu a na zistenie keyword
    dokončené KA pre string a čísla

--- scanner.c
+++ scanner.c
@@ -12,20 +12,20 @@
 
 #include "scanner.h"
 #include "err_code.h"
-#include <stdbool.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <ctype.h>
 
-tToken get_token(void) {
+tToken* get_token(void) {
         tState state;
-        tToken token;
         bool FirstToken;
         char c;
+        tToken *token = init_token();
+
         static int indent_cnt = 0;
         static int line_cnt = 0;
 
         state = sStart;       /* počiatocný stav */
-        stringInit(&token.data); /* inicializácia stringu */
 
         while(true) {
                 c = getchar();
@@ -35,12 +35,12 @@ tToken get_token(void) {
                         /****************počiatocný stav********************/
                         /* End Of File */
                         if(c == EOF) {
-                                stringAddString(&token.data, "EOF");
-                                token.line = line_cnt;
-                                token.type = sEOF;
+                                stringAddString(&token->data, "EOF");
+                                token->line = line_cnt;
+                                token->type = sEOF;
 
                                 return token;
-                        }
+                        } else
 
                         /* End Of Line */
                         if(c == '\n') {
@@ -51,11 +51,11 @@ tToken get_token(void) {
                                         FirstToken = true;
                                         line_cnt++;
 
-                                        stringAddChar(&token.data, '\n');
-                                        token.line = line_cnt;
-                                        token.type = sEOL;
+                                        stringAddChar(&token->data, '\n');
+                                        token->line = line_cnt;
+                                        token->type = sEOL;
                                 }
-                        }
+                        } else
 
                         /* space */
                         if(isspace(c)) {
@@ -66,15 +66,16 @@ tToken get_token(void) {
                                 } else {
                                         state = sStart;
                                 }
-                        }
+                        } else
 
                         /* line comment */
                         if(c == '#') {
                                 state = sLineComment;
-                        }
+                        } else
 
                         /* block comment */
                         if(c == '"') {
+                                token->line = line_cnt;
                                 /* 1st " */
                                 c = getchar();
                                 if(c == '"') {
@@ -90,27 +91,47 @@ tToken get_token(void) {
                                 } else {
                                         state = sLexError;
                                 }
-                        } else {
-                                state = sLexError;
-                        }
+                        } else
 
-                        /* id/keyword */
+                        /* string */
                         if(c == '\'') {
-                                token.line = line_cnt;
+                                token->line = line_cnt;
 
                                 state = sStringStart;
+                        }
+
+                        /* id/key */
+                        if(isalpha(c)) {
+                                token->line = line_cnt;
+                                stringAddChar(&token->data, c);
+
+                                state = sIdentificatorOrKeyword;
+                        } else
+                        if(c == '_') {
+                                token->line = line_cnt;
+                                stringAddChar(&token->data, c);
+
+                                state = sIdentificator;
+                        } else
+
+                        /* number */
+                        if(isdigit(c)) {
+                                token->line = line_cnt;
+                                stringAddChar(&token->data, c);
+
+                                state = sIntegerOrDouble;
                         }
 
                         break;
                 /**************koniec počiatočného stavu*************/
 
 
-
+                //TODO
                 /************** EOL Start ****************/
                 case sEOL:
 
                         break;
-                /************** EOL Start ****************/
+                /************** EOL End ****************/
 
                 /************** Line Comment Start ****************/
                 case sLineComment:
@@ -150,13 +171,15 @@ tToken get_token(void) {
                         break;
                 /************** Block Comment End ****************/
 
+
+                /********* String Start**************************************/
                 /************** String Start Start (lol)****************/
                 case sStringStart:
 
                         if(c == '\'') {
                                 /* prázdny string */
-                                stringAddString(&token.data, "");
-                                token.type = sString;
+                                stringAddString(&token->data, "");
+                                token->type = sString;
                         } else
                         if(c == '\\') {
                                 /* escape sequence */
@@ -164,7 +187,7 @@ tToken get_token(void) {
                         } else
                         if(c > 31) {
                                 /* literál */
-                                stringAddChar(&token.data, c);
+                                stringAddChar(&token->data, c);
                                 state = sStringRead;
                         } else {
                                 /* error */
@@ -176,29 +199,30 @@ tToken get_token(void) {
 
                 /************** String Escape Start ****************/
                 case sStringEscape:
+
                         switch(c) {
                         case 'n':
-                                stringAddChar(&token.data, '\n');
+                                stringAddChar(&token->data, '\n');
                                 state = sStringRead;
                                 break;
 
                         case 't':
-                                stringAddChar(&token.data, '\t');
+                                stringAddChar(&token->data, '\t');
                                 state = sStringRead;
                                 break;
 
                         case '\'':
-                                stringAddChar(&token.data, '\'');
+                                stringAddChar(&token->data, '\'');
                                 state = sStringRead;
                                 break;
 
                         case '\"':
-                                stringAddChar(&token.data, '\"');
+                                stringAddChar(&token->data, '\"');
                                 state = sStringRead;
                                 break;
 
                         case '\\':
-                                stringAddChar(&token.data, '\\');
+                                stringAddChar(&token->data, '\\');
                                 state = sStringRead;
                                 break;
 
@@ -207,13 +231,205 @@ tToken get_token(void) {
                                 break;
 
                         default:
-                                stringAddChar(&token.data, '\\');
-                                stringAddChar(&token.data, c);
+                                stringAddChar(&token->data, '\\');
+                                stringAddChar(&token->data, c);
                                 state = sStringRead;
                         }
 
                         break;
-                        /************** String Escape Start ****************/
+                /************** String Escape End ****************/
+
+                /************** String Escape Hex Start ****************/
+                case sStringEscapeHex:
+
+                        if((isdigit(c)) || ((c >= 65) && (c <= 70)) || ((c >= 97) && (c <= 102))) {
+                                /*   0-9                   A-F                         a-f   */
+                                char temp = c;
+                                c = getchar();
+
+                                if((isdigit(c)) || ((c >= 65) && (c <= 70)) || ((c >= 97) && (c <= 102))) {
+                                        /* escape success - return xx */
+                                        stringAddChar(&token->data, temp);
+                                        stringAddChar(&token->data, c);
+                                        state = sStringRead;
+                                } else {
+                                        /* escape failed - return \xx */
+                                        stringAddChar(&token->data, '\\');
+                                        stringAddChar(&token->data, temp);
+                                        stringAddChar(&token->data, c);
+                                        state = sStringRead;
+                                }
+
+                        } else {
+                                /* escape failde return \x */
+                                stringAddChar(&token->data, '\\');
+                                stringAddChar(&token->data, 'x');
+                                stringAddChar(&token->data, c);
+                                state = sStringRead;
+                        }
+
+                        break;
+                /************** String Escape Hex End ****************/
+
+                /************** String Read Start ****************/
+                case sStringRead:
+
+                        if(c == '\'') {
+                                /* koniec stringu */
+
+                                return token;
+                        } else
+                        if(c == '\\') {
+                                /* escape sequence */
+                                state = sStringEscape;
+                        } else
+                        if(c > 31) {
+                                /* literál */
+                                stringAddChar(&token->data, c);
+                                state = sStringRead;
+                        } else {
+                                /* error */
+                                state = sLexError;
+                        }
+
+                        break;
+                /************** String Read End ****************/
+                /*********String End *****************************/
+
+
+                /********* Keyword / ID Start ******************/
+                /************** ID/Key Start ****************/
+                case sIdentificatorOrKeyword:
+                        if(isalpha(c)) {
+                                stringAddChar(&token->data, c);
+                                state = sIdentificatorOrKeyword;
+                        } else
+                        if(isdigit(c) || c == '_') {
+                                stringAddChar(&token->data, c);
+                                state = sIdentificator;
+                        } else {
+                                /* koniec id/key - return */
+                                ungetc(c, stdin);
+
+                                assignType(token);
+                                return token;
+                        }
+                        assignType(token);
+                        break;
+                /************** ID/Key End ****************/
+
+                /************** ID Start ****************/
+                case sIdentificator:
+                        if(isalnum(c) || c == '_') {
+                                stringAddChar(&token->data, c);
+                                state = sIdentificator;
+                        } else {
+                                /* koniec id - return */
+                                ungetc(c, stdin);
+
+                                token->type = sIdentificator;
+                                return token;
+                        }
+                        break;
+                /************** ID End ****************/
+
+                /********* Keyword / ID End ******************/
+
+                /********* Number Start**************************************/
+                /************** Int/Double Start ****************/
+                case sIntegerOrDouble:
+                        if(isdigit(c)) {
+                                stringAddChar(&token->data, c);
+
+                                state = sIntegerOrDouble;
+                        } else
+                        if(c == '.') {
+                                stringAddChar(&token->data, c);
+
+                                state = sDoublePoint;
+                        } else
+                        if((c == 'E') || (c == 'e')) {
+                                stringAddChar(&token->data, c);
+
+                                state = sDoubleExponent;
+                        } else {
+                                /* koniec number - return integer */
+                                ungetc(c, stdin);
+
+                                token->type = sInteger;
+                                return token;
+                        }
+                        break;
+                /************** Int/Double End ****************/
+
+                /************** Double Point Start ****************/
+                case sDoublePoint:
+                        if(isdigit(c)) {
+                                stringAddChar(&token->data, c);
+
+                                state = sDoublePointNumber;
+                        } else {
+                                state = sLexError;
+                        }
+                        break;
+                /************** Double Point End ****************/
+
+                /************** EOL Start ****************/
+                case sDoubleExponentNumber:
+                        if(isdigit(c)) {
+
+                        }
+                        break;
+                        /************** EOL End ****************/
+
+                        /********* Number End ******************/
+
                 }
+        }
+}
+
+/* inicializácia tokenu */
+tToken* init_token(void) {
+  tToken* tmp = (tToken*) malloc(sizeof(struct structToken));
+
+  if(tmp == NULL) {
+          fprintf(stderr, "Chyba %d - chyba alokácie pamäte\n", ERR_INTERN);
+          exit(ERR_INTERN);
+  }
+
+  tmp->line = -1;
+  tmp->type = -1;
+  tmp->subtype = -1;
+  stringInit(&tmp->data);
+
+  return tmp;
+}
+
+
+/* funkcia zistí typ tokenu podľa obsahu data */
+void assignType(tToken* t) {
+        if(strcmp(t->data.value, "def") == 0) {
+                t->type = sDef;
+        } else
+        if(strcmp(t->data.value, "else") == 0) {
+                t->type = sElse;
+        } else
+        if(strcmp(t->data.value, "if") == 0) {
+                t->type = sIf;
+        } else
+        if(strcmp(t->data.value, "None") == 0) {
+                t->type = sNone;
+        } else
+        if(strcmp(t->data.value, "pass") == 0) {
+                t->type = sPass;
+        } else
+        if(strcmp(t->data.value, "return") == 0) {
+                t->type = sReturn;
+        } else
+        if(strcmp(t->data.value, "while") == 0) {
+                t->type = sWhile;
+        } else {
+                /* nieje keyword */
+                t->type = sIdentificator;
         }
 }
